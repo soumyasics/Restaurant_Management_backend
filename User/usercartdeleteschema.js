195489722f4f075b56b5043c8cb85b38ffb4b4c0
Ammu-raj <ammuamz2001@gmail.com>
2024-04-15 15:07:05 +0530

    abhiram2 commit

--- User/usercartdeleteschema.js
+++
@@ -1,5 +0,0 @@
-// var mongoose = require('mongoose');
-// var usercartdelete = new mongoose.Schema({
-//   foodid:{ type:mongoose.Types.ObjectId,required:true,ref:"cartschema"}
-// });
-// module.exports = new mongoose.model("usercartdelete", usercartdelete);
